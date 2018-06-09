8a8fd52853b57c4a389c2751397eec4a11fc30a1
Your Name <you@example.com>
2018-06-09 11:26:27 +0200

    refactory

--- templates/find_all_tags_template.sh
+++ templates/find_all_tags_template.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
-find {{ basedir }} -name '*.xml'  \
-  | grep '\.comments\/' \
-  | sed 's? ?\\ ?g' \
-  |  xargs cat \
+find {{ basedir }} -name '*.xml' -print0 \
+  | xargs -0 cat \
   | grep category \
-  | sed 's/.*\<category\ value\=\"//g' \
-  | sed 's?\"/>??g' \
   | sort \
-  | uniq
+  | uniq \
+  | sed 's/.*\<category\ value\=\"//g' \
+  | sed 's?\"/>??g' 
 
 # as yaml fortm
 # find {{ basedir }} -name '*.xml'  \
